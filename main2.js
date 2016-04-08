e61f34686515fc83fc8c756dee49ee93fb8be944
bastien girschig <bgirschig@google.com>
2016-04-08 23:48:24 +0200

    created a "showTime" function and solved a bug in the formatInt function

--- main2.js
+++ main2.js
@@ -1,38 +1,18 @@
+var launchSession = document.getElementById('start');
+launchSession.addEventListener("click", launchtimer);
 
-
-var timeInput = document.getElementById('request');
-timeInput.addEventListener("submit", addtime);
-
-function addtime (){
-  var hours = document.getElementById('hour').value;
-  var minutes = document.getElementById('minute').value;
-  var seconds = document.getElementById('second').value;
-
+function showTime (hours, minutes, seconds){
   document.getElementById('H').innerHTML = formatInt(hours,2);
   document.getElementById('M').innerHTML = formatInt(minutes,2);
   document.getElementById('S').innerHTML = formatInt(seconds,2);
 }
 
-var launchSession = document.getElementById('start');
-launchSession.addEventListener("click", launchtimer);
-
 function launchtimer (){
-
   var hours = document.getElementById('hour').value;
   var minutes = document.getElementById('minute').value;
   var seconds = document.getElementById('second').value;
-
-  if (hours===""){
-    hours=0;
-  }
-
-  if (minutes===""){
-    minutes=0;
-  }
-
-  if (seconds===""){
-    seconds=0;
-  }
+  
+  showTime(hours, minutes, seconds);
 
   var hoursMs = hours * 3600000;
   var minutesMs = minutes * 60000;
@@ -43,28 +23,17 @@ function launchtimer (){
 
   var timeInterval = setInterval (function (){
     
-    var i=1000;
     if (resultDateMs===currentDateMs){
-      document.getElementById('H').innerHTML='00';
-      document.getElementById('M').innerHTML='00';
-      document.getElementById('S').innerHTML='00';
+      showTime(0,0,0)
     } else {
-      diffDateMs -= i;
+      diffDateMs -= 1000;
     }
 
     var resultHours = Math.floor( (diffDateMs/(1000*60*60)) % 24 );
     var resultMinutes = Math.floor( (diffDateMs/1000/60) % 60 );
     var resultSeconds = Math.floor( (diffDateMs/1000) % 60 );
 
-    document.getElementById('H').innerHTML = formatInt(resultHours, 2);
-    document.getElementById('M').innerHTML = formatInt(resultMinutes, 2);
-    document.getElementById('S').innerHTML = formatInt(resultSeconds, 2);
-
-    if(resultSeconds<10){
-      document.getElementById('S').innerHTML='0'+resultSeconds;
-    }
-    else{document.getElementById('S').innerHTML=resultSeconds;
-    }
+    showTime(resultHours, resultMinutes, resultSeconds);
 
     if(diffDateMs===0){
       clearInterval(timeInterval);
@@ -74,8 +43,10 @@ function launchtimer (){
 }
 
 function formatInt(time, size){
-  var count = (count>0) ? Math.log10(time) : 0;
+  if( !time || isNaN(time) ) time = 0;
+  var count = (time>0) ? Math.log10(time) : 0;
   var result = "";
   for (var i = 0; i < size-count-1; i++) result += "0";
+  console.log("["+time+"] "+count);
   return result+time;
 }
